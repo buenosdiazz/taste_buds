0ad22cf8920a1a49dfe5246ba683ac566f548a07
Caro Diaz <carolinadiaz017@gmail.com>
2016-07-23 10:52:21 -0400

    login /logout/ sign up set up

--- final_project/db/seeds.rb
+++ final_project/db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+list1= List.create(title:'netflix originals')
+caro = User.create(name:'Caro', email:'caro@caro.com', password:'password', password_confirmation:'password')
