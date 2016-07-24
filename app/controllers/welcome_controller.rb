d76d904d466a7938fb8deab30ed8cf9e09d45341
Caro Diaz <carolinadiaz017@gmail.com>
2016-07-24 17:06:48 -0400

    heroku

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,9 @@
+class WelcomeController < ApplicationController
+  def home
+    render 'home'
+  end 
+
+  def show
+   render 'show'
+  end 
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: { registrations: "registrations" }
+  get '/', to: 'welcome#home'
+
+  get '/show', to: 'welcome#show'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/users/:id', to: 'users#show'
+end
