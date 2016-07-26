582452cbdddb271231793be56ece357bc6faa95b
Caro Diaz <carolinadiaz017@gmail.com>
2016-07-26 11:55:24 -0400

    got rid of coffee script

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable 
   has_many :tables
-  has_attached_file :avatar,:styles => { :medium => "300x300#", :thumb => "100x100#" }, :convert_options => { :all => '-auto-orient' }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar,:styles => { :medium => "300x300#", :thumb => "100x100#" }, :convert_options => { :all => '-auto-orient' }, :default_url => "/public/backup.jpg"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
